81c85c0c9e5ba5246fd453a88b7f535a44658f12
Daria Glebova <glebova555dasha@gmail.com>
2025-10-07 23:47:30 +0500

    Сделано 3 Задание

--- src/main/java/ru/naumen/collection/task3/WarAndPeace.java
+++ src/main/java/ru/naumen/collection/task3/WarAndPeace.java
@@ -1,6 +1,7 @@
 package ru.naumen.collection.task3;
 
 import java.nio.file.Path;
+import java.util.*;
 
 /**
  * <p>Написать консольное приложение, которое принимает на вход произвольный текстовый файл в формате txt.
@@ -19,10 +20,30 @@ public class WarAndPeace
             "Лев_Толстой_Война_и_мир_Том_1,_2,_3,_4_(UTF-8).txt");
 
     public static void main(String[] args) {
+
+        Map<String, Integer> wordCount = new HashMap<>();
+
         new WordParser(WAR_AND_PEACE_FILE_PATH)
                 .forEachWord(word -> {
-                    // TODO ваше действие над word
+                    //Функция hashcode(определена в string) гарантирует O(1) сложность в операции
+                    wordCount.merge(word, 1, Integer::sum);
                 });
-        // TODO выполнить задачу
+
+        //Нужен список, для полной сортровки
+        //Полная сортировка имеет сложность O(n log n)
+        //Но тк в файле мало данных + многие результаты частот дублируются - лучшая производительность
+        List<Map.Entry<String, Integer>> sortedWords = new ArrayList<>(wordCount.entrySet());
+        sortedWords.sort(Map.Entry.<String, Integer>comparingByValue().reversed());
+
+        //Быстрый доступ по индексу O(1)
+        System.out.println("TOP 10 самых частых слов:");
+        sortedWords.stream().limit(10).forEach(entry ->
+                System.out.println(entry.getKey() + ": " + entry.getValue())
+        );
+
+        System.out.println("\nLAST 10 самых редких слов:");
+        sortedWords.stream().skip(sortedWords.size() - 10).forEach(entry ->
+                System.out.println(entry.getKey() + ": " + entry.getValue())
+        );
     }
 }
